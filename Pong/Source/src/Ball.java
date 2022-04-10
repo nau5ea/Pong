35f715952ed66518d8b2eddccb22aceae5057e05
Louis Moench IV <49795953+nau5ea@users.noreply.github.com>
2022-04-09 22:40:32 -0700

    added my first observations
    noted the portuguese comments. also noticed one method that's super obvious and one branch that executes the same code in either case

--- Pong/Source/src/Ball.java
+++ Pong/Source/src/Ball.java
@@ -15,10 +15,10 @@ public class Ball {
 	private final int WIDTH = 5;
 	private final int HEIGHT = 5;
 	
-	Ball(){
+	Ball() {//not given a scope classifier or a return type?
 		this.x = Game.WIDTH/2;
 		this.y = 40;
-		
+		//from this line to the end of the method can be extracted to a new method
 		angle = new Random().nextInt(120 - 60) + 60 + 1;
 		
 		while(angle<110 && angle>70) {
@@ -29,7 +29,7 @@ public class Ball {
 		this.dy = Math.sin(Math.toRadians(angle));
 	}
 	
-	public void tick(){
+	public void tick() {
 		
 		x +=  dx * SPEED;
 		y += dy * SPEED;
@@ -37,48 +37,48 @@ public class Ball {
 		// Ball Outside	
 		if(y >= Game.HEIGHT)
 		{
-			//Ponto do inimigo.
-			System.out.println("Enemy's point!");
+			//Enemy location.
+			System.out.println("Enemy's location!");
 			new Game().start();
 			return;
-		}else if(y <= 0) {
-			//Ponto do jogador.
-			System.out.println("Player's point");
+		} else if (y <= 0) {
+			//Player location.
+			System.out.println("Player's location!");
 			new Game().start();
 			return;
 		}
 		
-		if(x<=0 || x>=Game.WIDTH - WIDTH)
-			dx*= -1;
+		if (x <= 0 || x >= Game.WIDTH - WIDTH)//this operator might be obscure. WIDTH should have a more descriptive name
+			dx *= -1;//Negate dx, reversing x velocity
 		
-		// Colisions
-		Rectangle bounds = new Rectangle((int)x,(int)y,WIDTH,HEIGHT);
-		Rectangle boundsPlayer = new Rectangle(Game.player.x, Game.player.y, Game.player.WIDTH, Game.player.HEIGHT);
-		Rectangle boundsEnemy = new Rectangle((int)Game.enemy.x, Game.enemy.y, Game.player.WIDTH, Game.player.HEIGHT);
+		// Collisions
+		Rectangle bounds = new Rectangle((int)x,(int)y,WIDTH,HEIGHT);//what does this represent
+		Rectangle boundsPlayer = new Rectangle(Game.player.x, Game.player.y, Game.player.WIDTH, Game.player.HEIGHT);//Game.player should be saved with a better name, and a better method can be written to replace Ractangle's constructor that just accepts the player object and pulls out this data
+		Rectangle boundsEnemy = new Rectangle((int)Game.enemy.x, Game.enemy.y, Game.player.WIDTH, Game.player.HEIGHT);//same for this
 		
-		if(bounds.intersects(boundsPlayer)) {
+		if (bounds.intersects(boundsPlayer)) {//what is bounds in this case?
 			angle = new Random().nextInt(120 - 60) + 61;
 			
-			while(angle<110 && angle>70) {
+			while(angle < 110 && angle > 70) {
 				angle = new Random().nextInt(120 - 60) + 61;
 			}
 			
 			this.dx = Math.sin(Math.toRadians(angle));	
-			this.dy = Math.cos(Math.toRadians(angle));	
+			this.dy = Math.cos(Math.toRadians(angle));	//this section seems familiar (lines 59 to here)
 			
-			if(dy>0)
+			if (dy > 0)//what if it's already negative? why can't we negate it?
 				dy *= -1; 
-		}else if(bounds.intersects(boundsEnemy)) {
+		} else if (bounds.intersects(boundsEnemy)) {//the only thing that differs after the branch is the condition for negating the y-velocity
 			angle = new Random().nextInt(120 - 60) + 61;
 			
-			while(angle<110 && angle>70) {
+			while (angle < 110 && angle > 70) {
 				angle = new Random().nextInt(120 - 60) + 61;
 			}
 			
 			this.dx = Math.sin(Math.toRadians(angle));	
 			this.dy = Math.cos(Math.toRadians(angle));	
 			
-			if(dy<0)
+			if (dy < 0)
 				dy *= -1;
 		}
 		
@@ -86,8 +86,8 @@ public class Ball {
 	
 	public void render(Graphics g){
 		tick();
-		g.setColor(new Color(255, 255, 255));
-		g.fillRect((int)x, (int)y, WIDTH, HEIGHT);
+		g.setColor(new Color(255, 255, 255));//has to be a better way to specify this color lol
+		g.fillRect((int) x, (int) y, WIDTH, HEIGHT);
 	}
 	
 }
